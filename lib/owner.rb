809d9aff712f2334e4fc44f1fef65cc78781b8e0
Samuel Cornet <samuel@noukod.com>
2018-10-05 18:35:00 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -52,7 +52,24 @@ class Owner
   def list_pets
     "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
   end
-
+  def walk_dogs
+     @@pets[:dogs].each do |dog|
+       dog.mood = 'happy'
+     end
+   end
+ 
+   def play_with_cats
+     @@pets[:cats].each do |cat|
+       cat.mood = 'happy'
+     end
+   end
+ 
+   def feed_fish
+     @@pets[:fishes].each do |fish|
+       fish.mood = 'happy'
+     end
+   end
+   
   def sell_pets
     pets.each do |pet, arr|
       arr.map do |pet|
