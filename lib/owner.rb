bd03ebb2561fe06c3097c0b6b44f234baa7203ac
Samuel Cornet <samuel@noukod.com>
2018-10-05 18:29:09 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,51 @@
 class Owner
-  # code goes here
+  attr_accessor :pets, :fishes, :cats, :dogs
+  attr_reader :species
+  @@ll = []
+  @@pets = {:fishes => [], :dogs => [], :cats => []}
+
+  def initialize(species)
+    # @species = species
+    # @name = name
+    # @@all << self
+  end
+
+
+  def self.all
+    @@ll
+  end
+
+  def pets
+    @@pets
+  end
+
+  def self.count
+     @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def buy_cat(cat)
+    @@pets[:cats] << Cat.new(cat)
+  end
+
+  def bug_dog(dog)
+    @@pets[:dogs] << Dog.new(dog)
+  end
+
+  def buy_fish(fish)
+    @@pets[fishes] << Cat.new(fish)
+  end
+
+  def feed_fish
+    @@pets[:fishes].each do |fish|
+      fish.mood = 'happy'
+    end
+  end
 end
